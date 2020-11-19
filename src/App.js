7ef28fe86c1db192eba18e2398367db1be870873
chikechris <cchike2017@gmail.com>
2020-11-19 06:06:57 -0500

    building out menu  comptent

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
-function App() {
-  return <h2>menu project setup</h2>;
+function App() { 
+  const [menuItems, setMenuItems] = useState(items)
+
+  return (
+    <main>
+      <section>
+        <div className='title'>
+            <h2>Our Menu</h2>
+            <div className="underline"></div>
+        </div>
+        <Categories/> 
+        <Menu items={menuItems}/>
+      </section>
+
+  </main>
+  );
 }
 
 export default App;
